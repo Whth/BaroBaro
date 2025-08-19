6a116c32964ab7518172d0162fbb718476214a34
Whth <whthyotta@qq.com>
2025-08-19 20:50:23 +0800

    feat(transmission): add serde derive to protobuf structs
    - Add serde::Serialize and serde::Deserialize derive macros to all protobuf structs
    - Update Cargo.toml to include serde dependency with derive feature- Modify build.rs to apply the derive macros to all generated structs

--- src-tauri/crates/transmission/build.rs
+++ src-tauri/crates/transmission/build.rs
@@ -1,6 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     prost_build::Config::new()
         .out_dir("src")
+        .type_attribute(
+            ".",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
         .compile_protos(
             &[
                 "../../../proto/transmission.proto"
