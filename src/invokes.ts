4388700538c5a421cbfd200f68936786098f3e1a
Whth <whthyotta@qq.com>
2025-08-21 01:24:00 +0800

    feat(api): add functions to interact with the Tauri API
    - Implement refresh_config, save_config, and list_installed_mods functions
    - Add config and installed_mod state management using Vue's ref
    - Import necessary modules for API interaction and data handling

---
+++ src/invokes.ts
@@ -0,0 +1,21 @@
+import {invoke} from "@tauri-apps/api/core";
+import {Config} from "./proto/config.ts";
+import {Ref, ref} from "vue";
+import {BarotraumaMod} from "./proto/mods.ts";
+
+const config = ref(Config.create())
+
+const installed_mod: Ref<BarotraumaMod[]> = ref([])
+
+
+export async function refresh_config() {
+    config.value = await invoke("read_config").then((data) => Config.fromJSON(data));
+}
+
+export async function save_config() {
+    await invoke("save_config", {config: Config.toJSON(config.value)});
+}
+
+export async function list_installed_mods() {
+    installed_mod.value = await invoke("list_installed_mods");
+}
