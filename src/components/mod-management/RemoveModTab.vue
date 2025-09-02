292a967e677305bb84f35df5378f5cbeb62df176
Whth <whthyotta@qq.com>
2025-09-02 15:47:17 +0800

    feat(mod-management): add mod item display component to RemoveModTab
    - Import and integrate ModItemDisplay component into RemoveModTab
    - Enhance visual representation of installed mods in the mod management section
    
    Signed-off-by: Whth <whthyotta@qq.com>

--- src/components/mod-management/RemoveModTab.vue
+++ src/components/mod-management/RemoveModTab.vue
@@ -2,7 +2,7 @@
   <n-scrollbar style="max-height: 60vh">
     <n-list :bordered="false" style="background-color: transparent">
       <n-list-item v-for="mod in installed_mod" :key="mod.steamWorkshopId">
-
+        <ModItemDisplay :mod="mod"/>
       </n-list-item>
     </n-list>
   </n-scrollbar>
@@ -10,6 +10,7 @@
 
 <script lang="ts" setup>
 import { installed_mod } from "../../invokes.ts";
+import ModItemDisplay from "./ModItemDisplay.vue";
 </script>
 
 
