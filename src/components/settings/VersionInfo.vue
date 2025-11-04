aff6f65a1c167b10734ab154b4241594b1b6599b
Whth <whthyotta@qq.com>
2025-11-04 20:28:58 +0800

    chore(version): bump version to 0.1.7 and update identifiers
    - Updated Cargo.toml version from 0.1.6 to0.1.7
    - Changed tauri.conf.json productName from barobaro to BaroBaro- Updated tauri.conf.json identifier from com.porgandia.barobaro to BaroBaro
    - Adjusted formatting in VersionInfo.vue template and script sections- Maintained all existing functionality and UI structure in version info display
    
    Signed-off-by: Whth <whthyotta@qq.com>

--- src/components/settings/VersionInfo.vue
+++ src/components/settings/VersionInfo.vue
@@ -1,97 +1,95 @@
 <template>
-  <n-card>
-    <n-grid :cols="3" :x-gap="16" :y-gap="16" class="animate-fade-in">
-      <!-- Game Mod Manager Card -->
-      <n-gi>
-        <n-card class="animate-slide-in-right" hoverable size="small">
-          <template #header>
-            <n-space align="center">
-              <n-icon size="20">
-                <GameController/>
-              </n-icon>
-              <n-text>{{ $t('version.title') }}</n-text>
-            </n-space>
-          </template>
-          <n-descriptions :column="1" label-placement="left" size="small">
-            <n-descriptions-item :label="$t('version.version')">
-              <n-text v-text="build_info.version"></n-text>
-            </n-descriptions-item>
-            <n-descriptions-item :label="$t('version.commit')">
-              <n-text v-text="build_info.commit"></n-text>
-            </n-descriptions-item>
-            <n-descriptions-item :label="$t('version.release_date')">
-              <n-text v-text="build_info.date"></n-text>
-            </n-descriptions-item>
-          </n-descriptions>
-        </n-card>
-      </n-gi>
-
-      <!-- About Card -->
-      <n-gi>
-        <n-card class="animate-slide-in-right" hoverable size="small">
-          <template #header>
-            <n-space align="center">
-              <n-icon size="20">
-                <InformationCircle/>
-              </n-icon>
-              <n-text>{{ $t('version.about') }}</n-text>
-            </n-space>
-          </template>
-          <n-space vertical>
-            <n-text>{{ $t('version.about_text_1') }}</n-text>
-            <n-text>{{ $t('version.about_text_2') }}</n-text>
+  <n-grid :cols="3" :x-gap="16" :y-gap="16" class="animate-fade-in">
+    <!-- Game Mod Manager Card -->
+    <n-gi>
+      <n-card class="animate-slide-in-right" hoverable size="small">
+        <template #header>
+          <n-space align="center">
+            <n-icon size="20">
+              <GameController/>
+            </n-icon>
+            <n-text>{{ $t('version.title') }}</n-text>
           </n-space>
-        </n-card>
-      </n-gi>
+        </template>
+        <n-descriptions :column="1" label-placement="left" size="small">
+          <n-descriptions-item :label="$t('version.version')">
+            <n-text v-text="build_info.version"></n-text>
+          </n-descriptions-item>
+          <n-descriptions-item :label="$t('version.commit')">
+            <n-text v-text="build_info.commit"></n-text>
+          </n-descriptions-item>
+          <n-descriptions-item :label="$t('version.release_date')">
+            <n-text v-text="build_info.date"></n-text>
+          </n-descriptions-item>
+        </n-descriptions>
+      </n-card>
+    </n-gi>
 
-      <!-- Links & Support Card -->
-      <n-gi>
-        <n-card class="animate-slide-in-right" hoverable size="small">
-          <template #header>
-            <n-space align="center">
-              <n-icon size="20">
-                <Link/>
-              </n-icon>
-              <n-text>{{ $t('version.links') }}</n-text>
-            </n-space>
-          </template>
-          <n-space vertical>
-            <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro')">
-              {{ $t('version.repository') }}
-            </n-button>
-            <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro/blob/master/README.md')">
-              {{ $t('version.documentation') }}
-            </n-button>
-            <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro/discussions')">
-              {{ $t('version.support') }}
-            </n-button>
-            <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro/issues')">
-              {{ $t('version.report_issues') }}
-            </n-button>
+    <!-- About Card -->
+    <n-gi>
+      <n-card class="animate-slide-in-right" hoverable size="small">
+        <template #header>
+          <n-space align="center">
+            <n-icon size="20">
+              <InformationCircle/>
+            </n-icon>
+            <n-text>{{ $t('version.about') }}</n-text>
           </n-space>
-        </n-card>
-      </n-gi>
+        </template>
+        <n-space vertical>
+          <n-text>{{ $t('version.about_text_1') }}</n-text>
+          <n-text>{{ $t('version.about_text_2') }}</n-text>
+        </n-space>
+      </n-card>
+    </n-gi>
 
-      <!-- Copyright Card -->
-      <n-gi>
-        <n-card class="animate-slide-in-right" hoverable size="small">
-          <template #header>
-            <n-space align="center">
-              <n-icon size="20">
-                <DocumentText/>
-              </n-icon>
-              <n-text>{{ $t('version.copyright') }}</n-text>
-            </n-space>
-          </template>
-          <n-space vertical>
-            <n-text>{{ $t('version.copyright_text') }}</n-text>
-            <n-text>{{ $t('version.license') }}</n-text>
-            <n-text depth="3">{{ $t('version.disclaimer') }}</n-text>
+    <!-- Links & Support Card -->
+    <n-gi>
+      <n-card class="animate-slide-in-right" hoverable size="small">
+        <template #header>
+          <n-space align="center">
+            <n-icon size="20">
+              <Link/>
+            </n-icon>
+            <n-text>{{ $t('version.links') }}</n-text>
           </n-space>
-        </n-card>
-      </n-gi>
-    </n-grid>
-  </n-card>
+        </template>
+        <n-space vertical>
+          <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro')">
+            {{ $t('version.repository') }}
+          </n-button>
+          <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro/blob/master/README.md')">
+            {{ $t('version.documentation') }}
+          </n-button>
+          <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro/discussions')">
+            {{ $t('version.support') }}
+          </n-button>
+          <n-button text type="primary" @click="openUrl('https://github.com/Whth/BaroBaro/issues')">
+            {{ $t('version.report_issues') }}
+          </n-button>
+        </n-space>
+      </n-card>
+    </n-gi>
+
+    <!-- Copyright Card -->
+    <n-gi>
+      <n-card class="animate-slide-in-right" hoverable size="small">
+        <template #header>
+          <n-space align="center">
+            <n-icon size="20">
+              <DocumentText/>
+            </n-icon>
+            <n-text>{{ $t('version.copyright') }}</n-text>
+          </n-space>
+        </template>
+        <n-space vertical>
+          <n-text>{{ $t('version.copyright_text') }}</n-text>
+          <n-text>{{ $t('version.license') }}</n-text>
+          <n-text depth="3">{{ $t('version.disclaimer') }}</n-text>
+        </n-space>
+      </n-card>
+    </n-gi>
+  </n-grid>
 </template>
 
 <script lang="ts" setup>
